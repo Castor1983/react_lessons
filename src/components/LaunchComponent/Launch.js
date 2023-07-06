077a2914817e03258dbd64b2e8dfdc24d793ff98
Castor1983 <a0958086750@gmail.com>
2023-07-06 20:02:37 +0300

    posts commit

--- src/components/LaunchComponent/Launch.js
+++ src/components/LaunchComponent/Launch.js
@@ -6,7 +6,7 @@ const Launch = (props) => {
         <div className={styles.container}>
             <h6>Mission name: {launch.mission_name}</h6>
             <h6>Launch year: {launch.launch_year}</h6>
-            <img src ={launch.links.mission_patch_small} alt={launch.mission_name} </>
+            <img src ={launch.links.mission_patch_small} alt={launch.mission_name} />
 
         </div>
     )
