116aa4ba96aba3139336d3d27d400b28d45e4750
Castor1983 <a0958086750@gmail.com>
2023-07-11 18:23:21 +0300

    last commit

--- src/components/Child2_2/Child2_2.js
+++ src/components/Child2_2/Child2_2.js
@@ -4,7 +4,7 @@ import {TransferContext} from "../../App";
 const Child2_2 = () => {
     const {setInfo} = useContext(TransferContext)
     const  text = 'Transfer from the Child2_2 to the Child1_1'
-    useEffect(()=> setInfo(text))
+    useEffect(()=> setInfo(text), [])
     return (
 
             <div>
