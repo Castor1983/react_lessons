3df0b29e6ba546782c26ca627bf0ac76e126be66
Castor1983 <a0958086750@gmail.com>
2023-07-11 15:23:24 +0300

    last commit

--- src/components/UsersComponent/UsersComponent.js
+++ src/components/UsersComponent/UsersComponent.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {UserComponent} from "../UserComponent/UserComponent";
 
-const UsersComponent = ({users, setUserId, setName, showPosts, setShowPosts}) => {
-
+const UsersComponent = ({setUserId, setName, showPosts, setShowPosts}) => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => setUsers(users))
+    }, [])
     return (
         <div>
             {users.map(user => <UserComponent key={user.id}
