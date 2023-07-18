788c9cb06475ba8838992731df53e4001be456cb
Castor1983 <a0958086750@gmail.com>
2023-07-18 16:26:47 +0300

    first commit

--- src/api/Endpoints.js
+++ src/api/Endpoints.js
@@ -1,5 +1,6 @@
 export const Endpoints = {
-    USERS: '/users'
+    USERS: '/users',
+    COMMENTS: '/comments'
 }
 
 export const baseUrl = 'https://jsonplaceholder.typicode.com'
--- src/services/ApiServices/ApiServices.js
+++ src/services/ApiServices/ApiServices.js
@@ -1,26 +1,23 @@
 import {Endpoints} from "../../api/Endpoints";
 import {baseUrl} from "../../api/Endpoints";
 import axios from "axios";
-
+const instance = axios.create({
+    baseURL: baseUrl,
+    headers: {'Content-type': 'application/json'}
+});
 export const NewUserService = {
     createNewUser: (newUser) => {
-        axios.post(baseUrl + Endpoints.USERS, {
+        instance.post(Endpoints.USERS, {
             body: JSON.stringify(newUser),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
         })
-            .then((json) => console.log(json));
+            .then((json) => console.log(json.data));
     }
 }
 export const NewComment = {
     createNewComment: (newComment) => {
-        axios.post(baseUrl + Endpoints.USERS, {
+        instance.post(Endpoints.COMMENTS, {
             body: JSON.stringify(newComment),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
         })
-            .then((json) => console.log(json));
+            .then((json) => console.log(json.data));
     }
 }
