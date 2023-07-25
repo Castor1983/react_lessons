25ca739a7d21772cabb48d5389428528f7ea53cd
Castor1983 <a0958086750@gmail.com>
2023-07-25 23:05:15 +0300

    add css style

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {MoviesList} from "../../components/MoviesList/MoviesList";
 import css from './MoviesPage.module.css'
+import {Header} from "../../components/Header/Header";
 const MoviesPage = () => {
     return (
         <div className={css.MoviesPage}>
+            <Header/>
             <MoviesList/>
         </div>
     );
