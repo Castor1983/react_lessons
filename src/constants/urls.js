c48e45bcac985ff67b9bcc8487a3b9997d67d1af
Castor1983 <a0958086750@gmail.com>
2023-07-25 10:56:30 +0300

    first commit

---
+++ src/constants/urls.js
@@ -0,0 +1,17 @@
+const movieBaseURL = 'https://api.themoviedb.org/3';
+const genreBaseURL = 'https://api.themoviedb.org/3/genre/movie/list';
+const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
+
+const apiKey = '?api_key=99ccb4256a47dbd8636cab59076eb9ca';
+
+const urls = {
+    movieList: '/discover/movie',
+
+}
+export {
+    movieBaseURL,
+    genreBaseURL,
+    imageBaseURL,
+    apiKey,
+    urls
+}
