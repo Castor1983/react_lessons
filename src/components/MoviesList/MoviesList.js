33f8c946e4a8564f080319cf02b2e81d89b35ee1
Castor1983 <a0958086750@gmail.com>
2023-08-01 17:07:10 +0300

    end work genres badge

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,12 +4,12 @@ import {MoviesListCard} from "../MoviesListCard/MoviesListCard";
 import {moviesListService} from "../../services/moviesListService";
 import css from './MoviesList.module.css'
 import {Pagination} from "../Pagination/Pagination";
-import {number} from "prop-types";
+import {useNavigate} from "react-router-dom";
 
 const MoviesList = () => {
     const [movies, setMovies] = useState([]);
     const [numberPage, setNumberPage] = useState([])
-
+    const navigate = useNavigate()
     useEffect(() => {
         moviesListService.getAll(numberPage).then(({data}) => setMovies(data.results))
 
@@ -17,9 +17,19 @@ const MoviesList = () => {
 
     return (
         <div className={css.MoviesList}>
-            <button className={css.prevNext} onClick={()=> setNumberPage(() => numberPage -1)}>prev</button>
-            <Pagination setNumberPage={setNumberPage} />
-            <button className={css.prevNext} onClick={()=> setNumberPage(() => numberPage +1)}>next</button>
+            {numberPage > 1 ? (<button className={css.prevNext} onClick={() => {
+                    setNumberPage(() => numberPage - 1)
+                    navigate(`/movieslist/page/${numberPage - 1}`)
+                }}>prev</button>) :
+                <button disabled>PrevPage</button>}
+            <Pagination setNumberPage={setNumberPage}/>
+            <button className={css.prevNext} onClick={() => {
+                setNumberPage(() => numberPage + 1)
+                navigate(`/movieslist/page/${numberPage + 1}`)
+            }}>next
+            </button>
+
+
             {movies.map(movie => <MoviesListCard key={movie.id} movie={movie}/>)}
 
         </div>
