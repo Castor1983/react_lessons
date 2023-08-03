9da663d9cf7bcd1e9fbd76edf5f09304b3d2566f
Castor1983 <a0958086750@gmail.com>
2023-08-03 14:13:06 +0300

    useFetch commit

--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -6,7 +6,7 @@ function useFetch (endpoint) {
         fetch(`https://jsonplaceholder.typicode.com${endpoint}`)
             .then(resp => resp.json())
             .then(json => setData(json))
-    }, [])
+    }, [endpoint])
     return console.log(data)
 }
 export {useFetch}
