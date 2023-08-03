cf7e19c771283046598796dc74e46a2e780ce00b
Castor1983 <a0958086750@gmail.com>
2023-08-03 18:10:02 +0300

    useArray commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import {users} from "./usersArray/usersArray";
+
 import {User} from "./components/User/User";
+import {useArray} from "./hooks/useArray/useArray";
+import {useState} from "react";
 
 const App = () => {
-
+    const [defaultValue, setDefaultValue] = useState(null)
+const newUsers = useArray(1)
     return (
         <>
 
-            {users.map(user=><User key={user.id} user={user}/>)}
+            {newUsers.map(user=><User key={user.id} user={user}/>)}
         </>
     );
 };
--- src/hooks/useArray/useArray.js
+++ src/hooks/useArray/useArray.js
@@ -1,4 +1,13 @@
-function useArray(defaultValue) {
-    
+import {users} from "../../usersArray/usersArray";
+
+const useArray = (defaultValue) => {
+
+   if (typeof defaultValue === 'object') {
+       users.push(defaultValue)
+   } else if (typeof defaultValue === 'number') {
+      users.filter((user)=>user.id !== defaultValue)
+   }
+   return users
+
 }
 export {useArray}
