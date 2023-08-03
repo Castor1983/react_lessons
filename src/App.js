494b44d0f6e79b6b4c96a933cf9e4fae0e73e3e9
Castor1983 <a0958086750@gmail.com>
2023-08-03 14:10:08 +0300

    useFetch commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import {useFetch} from "./hooks/useFetch/useFetch";
 
 const App = () => {
+const users = '/users';
+const posts = '/posts';
+const comments = '/comments';
 
     return (
         <>
 
-            {useFetch('/users')}
-            {useFetch('/posts')}
-            {useFetch('/comments')}
+            {useFetch(users)}
+            {useFetch(posts)}
+            {useFetch(comments)}
         </>
     );
 };
--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -1,10 +1,12 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function useFetch (endpoint) {
+    const [data, setData] = useState([])
     useEffect(()=> {
         fetch(`https://jsonplaceholder.typicode.com${endpoint}`)
             .then(resp => resp.json())
-            .then(json => console.log(json))
-    })
+            .then(json => setData(json))
+    }, [])
+    return console.log(data)
 }
 export {useFetch}
