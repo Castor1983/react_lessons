ade58574eeec70c30cfcbfcc1129e7f75d867e5b
Castor1983 <a0958086750@gmail.com>
2023-08-09 06:28:47 +0300

    Redux commit

--- src/components/CarContainer/CarContainer.js
+++ src/components/CarContainer/CarContainer.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import {createContext, useState} from "react";
+
 import {CarForm} from "../CarForm/CarForm"
 import {Cars} from "../Cars/Cars";
 
-const Context = createContext(null);
 const CarContainer = () => {
-    const [carForUpdate, setCarForUpdate] = useState(null);
     return (
-        <Context.Provider value={{
-            carForUpdate,
-            setCarForUpdate
-        }}>
             <div>
                 <CarForm/>
                 <Cars/>
             </div>
-        </Context.Provider>
     );
 };
 
-export {CarContainer, Context};
+export {CarContainer};
